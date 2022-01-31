c07e74cf86d3b32bde2a9e1839df34f52729f6cd
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2022-01-31 13:36:32 -0300

    Projeto de lista de tarefas em React

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+    'react/forbid-prop-types': 0,
+  },
+};
